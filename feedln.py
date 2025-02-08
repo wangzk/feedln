9bcd0b19d500e2327bb5e6f61c01870c42ca7711
Zk Wang <2674020+wangzk@users.noreply.github.com>
2025-02-08 08:19:06 +0800

    Update the color scheme.

--- feedln.py
+++ feedln.py
@@ -1389,9 +1389,9 @@ def export_feed_entry_to_file(conn, feed_item, filename):
 def initialize_screen(stdscr, conn):
     curses.start_color()
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Color pair 1: White text on black background
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLUE)    # Color pair 2: Yellow text on blue background
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)    # Color pair 2: Yellow text on blue background
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_RED)
-    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_GREEN)
+    curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_GREEN) # Footer notification
     display_categories(stdscr, conn)  # Call the display function after initializing colors 
 
 # Main function
